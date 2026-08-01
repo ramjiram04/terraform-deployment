e33eabbc3b2fa9c9d87d2f46b02e4e2d714101ba
unknown <ramji.r@hcltech.com>
2026-08-02 00:26:05 +0530

    changes

--- versions.tf
+++ versions.tf
@@ -2,12 +2,11 @@ terraform {
   required_version = ">= 1.10.0"
 
   backend "s3" {
-    bucket         = "cloud360-agent-statefile"
-    key            = "terraform-generator/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    use_lockfile   = true
-    dynamodb_table = "terraform-state-lock"
+    bucket       = "cloud360-agent-statefile"
+    key          = "terraform-generator/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 
   required_providers {
